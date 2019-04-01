69595f58ed446da4650a163b428a5904793aceb1
thejoin <miki.lombi@gmail.com>
2019-04-01 23:40:26 +0200

    reverb test

--- init.py
+++ init.py
@@ -11,7 +11,7 @@ def calc_pan(index):
 
 #first_song = playlist_songs.pop(0)
 interval = 0.2 * 1000 # sec
-song = AudioSegment.from_mp3('mp3/castle-of-glass.mp3')
+song = AudioSegment.from_mp3('mp3/everlong.mp3')
 song_inverted = song.invert_phase()
 song.overlay(song_inverted)
 
@@ -21,34 +21,23 @@ song_start_point = 0
 print "split song in part"
 while song_start_point+interval < len(song):
     splitted_song.append(song[song_start_point:song_start_point+interval])
-    #splitted_song_inverted.append(song_inverted[song_start_point:song_start_point+interval])
     song_start_point += interval
 
 if song_start_point < len(song):
     splitted_song.append(song[song_start_point:])
-    #splitted_song_inverted.append(song[song_start_point:])
 
 print "end splitting"
 print "total pieces: " + str(len(splitted_song))
 
 ambisonics_song = splitted_song.pop(0)
 pan_index = 0
-#left_db_index = 45
-#right_db_index = 45
-index = 0
 for piece in splitted_song:
-    print "start panning pieces: " + str(index)
     pan_index += 5
     piece = piece.pan(calc_pan(pan_index))
-    #WN = WhiteNoise().to_audio_segment(interval).apply_gain(-10.5).pan(-calc_pan(pan_index))
-    #piece.overlay(splitted_song_inverted[index].pan(-calc_pan(pan_index)))
     ambisonics_song = ambisonics_song.append(piece, crossfade=interval/50)
-    index += 1
-    #left_db_index = calc_left(left_db_index, right_db_index)
-    #right_db_index = calc_right(right_db_index, left_db_index)
 
 
 # lets save it!
-out_f = open("compiled/castle-of-glass.mp3", 'wb')
+out_f = open("compiled/everlong.mp3", 'wb')
 
 ambisonics_song.export(out_f, format='mp3')
